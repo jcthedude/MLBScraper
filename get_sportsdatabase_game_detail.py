1786693bb08148834a136970cedf3874ff6e6c5c
Justin Campana <justin.campana@gmail.com>
2015-11-29 20:27:45 -0800

    Initial commit

---
+++ get_sportsdatabase_game_detail.py
@@ -0,0 +1,56 @@
+import mysql.connector
+from datetime import datetime
+import urllib3
+from bs4 import BeautifulSoup
+from globals import *
+
+
+def get_game_details():
+    valid_url = True
+    http = urllib3.PoolManager()
+
+    # make sure http request is valid
+    try:
+        r = http.request('GET', sports_database_url)
+    except:
+        valid_url = False
+        print("Problem with URL data returned.")
+        pass
+
+    # print details for console tracking
+    print("URL: ", sports_database_url)
+
+    # proceed with the process is there's a valid http response
+    if valid_url:
+        # soup the data returned from the http request
+        soup = BeautifulSoup(r.data, 'html.parser')
+
+        # setup soups
+        try:
+            soup_game_data = soup.find_all("table", {"id": "outer"})[0].find_all("table", {"id": "DT_Table"})[0].find_all('tr')
+            print(soup_game_data)
+        except IndexError:
+            soup_game_data = None
+            print("Index error")
+            pass
+
+        # parse soups
+        if soup_game_data is not None:
+            for row in soup_game_data:
+                print(row.get_text().strip())
+        else:
+            print("No data found")
+
+
+def db_read():
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+    cursor.execute("""SELECT * FROM Raw_GameDetail""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    connection.close()
+
+
+get_game_details()
---
+++ globals.py
@@ -0,0 +1,9 @@
+db_config = {
+  'user': 'slampana',
+  'password': 'Campana1',
+  'host': '45.55.23.232',
+  'database': 'mlb',
+  'raise_on_warnings': True,
+}
+
+sports_database_url = 'http://api.sportsdatabase.com/mlb/query?output=default&rl=1&sdql=season%2C+date%2C+day%2C+game+number%2C+series+game%2C+team%2C+runs%2C+wins%2C+losses%2C+streak%2C+matchup+wins%2C+rest%2C+site+streak%2C+starter+wins%2C+starter+losses%2C+starter+rest%2C+line%2C+profit%2C+o%3Ateam%2C+o%3Aruns%2C+o%3Awins%2C+o%3Alosses%2C+o%3Astreak%2C+matchup+losses%2C+o%3Arest%2C+o%3Asite+streak%2C+o%3Astarter+wins%2C+o%3Astarter+losses%2C+o%3Astarter+rest%2C+o%3Aline%2C+o%3Aprofit%2C+ou+margin%2C+over%2C+under%2C+ou+streak+%40+season%3D2014+and+site%3Dhome&submit=++S+D+Q+L+%21++'
